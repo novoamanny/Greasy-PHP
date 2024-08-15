a605408f14a83defaa1c0cc87f1cc513ba3be143
skylerstyron <skylerstyrondev@gmail.com>
2024-08-15 09:58:26 -0500

    Added Join The Society files.

---
+++ join-the-society.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Join The Society ACF
+ */
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ */
+get_header();
+
+?>
+<!-- Join The Society Page -->
+<div id="JTS" class="JTS">
+    <?php include 'components/global-section-hero.php' ?>
+
+</div>
+
+
+
+<?php
+get_footer();
